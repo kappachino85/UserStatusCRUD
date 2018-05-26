dbd0c91031e0aa35011666aa812a464bed0da20d
Jae Phan <kappachino85@gmail.com>
2018-05-26 03:11:50 -0700

    Created javascript file and a getall function with ajax

---
+++ UserStatusCRUD/Scripts/main.js
@@ -0,0 +1,39 @@
+﻿$(document).ready(function () {
+    
+    getAllUsers();
+
+    var data = {};
+
+    //gets all the users and status
+    function getAllUsers() {
+        var url = "/api/users";
+
+        var settings = {
+            cache: false,
+            dataType: "json",
+            type: "GET",
+            success: getAllSuccess,
+            error: getAllError 
+        };
+
+        $.ajax(url, settings);
+    }
+
+    function getAllSuccess(rdata) {
+        $.each(rdata, function () {
+            var uName = this.UserName;
+            if (this.Status == 1) {
+                $('.online').append('<div class="onTemplate">' + uName + ' is currently online.</div>');
+            }
+            else if (this.Status == 0) {
+                $('.offline').append('<div class="offTemplate">' + uName + ' is currently offline.</div>');
+            }
+        });
+    }
+
+    function getAllError() {
+        console.log("error");
+    }
+
+});
+
