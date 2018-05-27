bafa1c41d163bc8b2b6056669fd0e8c1dbceca6e
Jae Phan <kappachino85@gmail.com>
2018-05-27 02:15:45 -0700

    More debugging

--- UserStatusCRUD/Scripts/main.js
+++ UserStatusCRUD/Scripts/main.js
@@ -1,6 +1,15 @@
 ﻿$(document).ready(function () {
-    console.log('hi');
+
     getAllUsers();
+    $("#submitButton").click(function () {
+        console.log('clickedSubmit');
+    });
+
+    //because the templates are created dynamically, a regular click handler will not work. it must be done like this:
+    $('body').on('click', 'a.nameLink', function () {
+        console.log(this);
+        console.log('clicked2');
+    });
 
     var data = {};
 
@@ -23,11 +32,12 @@
         $.each(data, function () {
             console.log(this.Status);
             var uName = this.UserName;
+            var id = this.Id;
             if (this.Status == "online") {
-                $('.online').append('<div class="onTemplate">' + uName + ' is currently online.</div>');
+                $('.online').append('<div class="onTemplate">' + id + '<a class="nameLink"> ' + uName + '</a> is currently online.</div>');
             }
             else if (this.Status == "offline") {
-                $('.offline').append('<div class="offTemplate">' + uName + ' is currently offline.</div>');
+                $('.offline').append('<div class="offTemplate">' + id + '<a class="nameLink"> ' + uName + '</a> is currently offline.</div>');
             }
         });
     }
@@ -37,4 +47,3 @@
     }
 
 });
-
